6051c4eff54bf9147c23022d6625d9eacc5f3043
Germano Kuerten <germanokuerten@Germanos-MBP.lan>
2022-04-23 12:20:07 -0700

    update

--- server.js
+++ server.js
@@ -33,6 +33,14 @@ cxn
 // Schemas and Models
 //////////////////////////////
 
+// Schema the definition of our data type
+// Model, the object for working with our data type
+const todoSchema = new mongoose.Schema({
+    text: String,
+    completed: Boolean   
+},  {timestamps: true})
+
+const Todo = mongoose.model("todo", todoSchema)
 
 //////////////////////////////
 // Create Express Application
@@ -55,8 +63,32 @@ app.use("/static", express.static("static")) // serve files statically
 // Routes 
 //////////////////////////////
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hello World</h1>")
+// test route
+// app.get("/", (req, res) => {
+//     res.send("<h1>Hello World</h1>")
+// })
+
+app.get("/", async (req, res) => {
+    // go get todos
+    const todos = await Todo.find({}).catch((err) => res.send(err))
+    // render index.ejs
+    res.render("index.ejs", {todos})
+})
+
+// async tells JS this is a asyncronous code
+app.get("/todo/seed", async (req, res) => {
+    // Todo.remove({}), (err, results)
+
+    // delete all existing todos
+    await Todo.remove({}).catch((err) => res.send(err))
+    //add your sample todos
+    const todos = await Todo.create([
+        {text: "eat breakfast", completed: false},
+        {text: "eat lunch", completed: false},
+        {text: "eat dinner", completed: false}
+    ]).catch((err) => res.send(err))
+    // send the todos as json
+    res.json(todos)
 })
 
 
